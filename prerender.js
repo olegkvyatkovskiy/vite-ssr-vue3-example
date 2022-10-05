8a678b1eca657bb5d0d68dc2c92a87d31e37a250
Oleg <oleg.kvyatkovsky@gmail.com>
2022-10-06 00:12:44 +0300

    initial comit

---
+++ prerender.js
@@ -0,0 +1,44 @@
+// Pre-render the app into static HTML.
+// run `npm run generate` and then `dist/static` can be served as a static site.
+
+import fs from 'node:fs'
+import path from 'node:path'
+// eslint-disable-next-line import/no-nodejs-modules
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const toAbsolute = (p) => path.resolve(__dirname, p)
+
+const manifest = (await import('./dist/static/ssr-manifest.json',{ assert: { type: "json" } })).default
+
+// process.exit()
+
+const template = fs.readFileSync(toAbsolute('dist/static/index.html'), 'utf-8')
+const { render } = await import('./dist/server/entry-server.js')
+
+// determine routes to pre-render from src/pages
+const routesToPrerender = fs
+  .readdirSync(toAbsolute('src/pages'))
+  .map((file) => {
+    const name = file.replace(/\.vue$/, '').toLowerCase()
+    return name === 'home' ? `/` : `/${name}`
+  })
+
+;(async () => {
+  // pre-render each route...
+  for (const url of routesToPrerender) {
+    const [appHtml, preloadLinks] = await render(url, manifest)
+
+    const html = template
+      .replace(`<!--preload-links-->`, preloadLinks)
+      .replace(`<!--app-html-->`, appHtml)
+
+    const filePath = `dist/static${url === '/' ? '/index' : url}.html`
+    fs.writeFileSync(toAbsolute(filePath), html)
+    console.log('pre-rendered:', filePath)
+  }
+
+  // done, delete ssr manifest
+  fs.unlinkSync(toAbsolute('dist/static/ssr-manifest.json'))
+})()
